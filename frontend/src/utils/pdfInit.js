973d8a9eb5799013fecfc0500d1e3742fab6feb5
Nabi <nabiumarov02@gmail.com>
2025-07-01 22:17:09 +0700

    fix pdfMake import path

--- frontend/src/utils/pdfInit.js
+++ frontend/src/utils/pdfInit.js
@@ -1,5 +1,5 @@
 import pdfMake from "pdfmake/build/pdfmake";
-import pdfFonts from "pdfmake/build/vfs_fonts.js";
+import pdfFonts from "pdfmake/build/vfs_fonts";
 
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
